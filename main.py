aa3cdf5321df5236dfdce45df18a9cfd5fa777ed
heung115 <heung115@naver.com>
2024-04-19 13:48:30 +0900

    ✨ ADD genimi api page

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI
 import os
+
 from supabase import create_client, Client
 from dotenv import load_dotenv
 
-# 환경변수
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-load_dotenv(os.path.join(BASE_DIR, ".env"))
+from gemini.single_turn import generate_response
+from env import getEnv
 
+# 환경변수
 app = FastAPI()
 
-url: str = os.environ["SUPABASE_URL"]
-key: str = os.environ["SUPABASE_KEY"]
+url: str = getEnv("SUPABASE_URL")
+key: str = getEnv("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 
@@ -23,3 +24,9 @@ async def root():
 async def read_items():
     data = supabase.table("nodes").select("NODE_NM").execute()
     return data.data
+
+
+@app.get("/gimini")
+async def read_goal(input: str = ""):
+    result = generate_response(input)
+    return result
