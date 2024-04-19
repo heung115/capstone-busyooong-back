22f6bc2cc1ab387373a945c43562f3be53625c73
GCocs <145097866+GCocs@users.noreply.github.com>
2024-04-19 21:41:54 +0900

    지도검색을 통한 주변 정류소 검색기능
    지도검색 api
    예제는 장소검색으로 만들었지만 도로명 주소와 지번주소, 행정구역으로도 가능하여 활용방안이 다양함. 뿐만아니라 그냥 주소를 좌표로 변환하는 것 또한 가능
    *배열에서는 0번을 예시로 들었지만 검색결과는 다양하였고 이러한 검색결과는 UI에서 선택할 수 있으면 좋을 것 같음
    https://www.vworld.kr/dev/v4dv_search2_s001.do
    
    주변 버스정류장 검색 api
    좌표 제공시 500m내에 있는 버스정류장을 모두 알려줌
    *배열에서 0번이 가장 근거리로 추정됨 (아직 확신은 아님)
    https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15098534

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 from fastapi import FastAPI
-import os
-from supabase import create_client, Client
-from dotenv import load_dotenv
+import requests
 
-# 환경변수
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-load_dotenv(os.path.join(BASE_DIR, ".env"))
+apiurl = "https://api.vworld.kr/req/search?"
+params = { #장소검색 기준, 도로명 주소, 지번주소 등 설정가능
+    "service": "search",
+    "request": "search",
+    "crs": "EPSG:4326",
+    "query": "경기대 수원캠", #목적지 설정
+    "type": "place",
+    "format": "json",
+    "key": "인증키", #인증키 부분 지우고 V-world(디지털트윈국토)에서 발급받은 api 인증키 입력
+}
+response = requests.get(apiurl, params=params)
+adressData = None
+if response.status_code == 200:
+    adressData = response.json()
+    print(response.json())
 
-app = FastAPI()
+    destinationPointLong = adressData.get("response").get("result").get("items")[0].get("point").get("x") #중간에 배열 0은 첫번째 결과값을 꺼낸 것이므로 나중에 선택가능하면 좋을 듯
+    destinationPointLati = adressData.get("response").get("result").get("items")[0].get("point").get("y") #중간에 배열 0은 첫번째 결과값을 꺼낸 것이므로 나중에 선택가능하면 좋을 듯
 
-url: str = os.environ["SUPABASE_URL"]
-key: str = os.environ["SUPABASE_KEY"]
-supabase: Client = create_client(url, key)
+# API URL 및 파라미터 설정
+url = "http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getCrdntPrxmtSttnList"
+params = {
+    "serviceKey": "인증키", #인증키 부분을 지우고 공공데이터 포털 api 인증키(decode) 입력
+    "gpsLati" : destinationPointLati,
+    "gpsLong" : destinationPointLong,
+    "numOfRows" : "10",
+    "pageNo" : "1",
+    "_type" : "json"
+}
 
+# GET 요청
+response = requests.get(url, params=params)
+busstopDataJson = response.json()
+busstopData = busstopDataJson.get("response").get("body").get("items").get("item")[0].get("nodenm") #배열 0~5까지있는데 가장 가까운건 0으로 추정, 괄호로 가능하지만 문제가 생길 수 있다고 해서 get함수 사용
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/nodes")
-async def read_items():
-    data = supabase.table("nodes").select("NODE_NM").execute()
-    return data.data
+# 응답 출력
+print(busstopData)
